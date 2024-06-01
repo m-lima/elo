8d01ba7f8fa06e81783fad61fe5db1845be04b32
Marcelo Lima <m-lima@users.noreply.github.com>
2024-06-01 22:36:07 +0200

    Add some icons

--- front/src/components/side.tsx
+++ front/src/components/side.tsx
@@ -1,28 +1,20 @@
 import { A } from "@solidjs/router";
+import { Suspense } from "solid-js";
+
+import trophy from '../assets/trophy.svg';
+import user from '../assets/user.svg';
+import { useSelf } from "../store";
 
 import './side.css';
 
-export const Side = () =>
-  <aside class='components side root'>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>Haaaa</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-    <A href='/'>H</A>
-  </aside>;
+export const Side = () => {
+  const self = useSelf();
+
+  return (
+    <aside class='components side root'>
+      <A href='/'><img src={trophy} /></A>
+      <A href={self() ? `/user'${self()!.id}` : window.location}><img src={user} /></A>
+      <A href='/'>h</A>
+    </aside >
+  );
+}

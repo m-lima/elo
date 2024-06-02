26558991a9d903809f68f6c775e9fd8612a5bb02
Marcelo Lima <m-lima@users.noreply.github.com>
2024-06-02 14:39:15 +0200

    WIP: Improve sidebar

--- front/src/components/side.tsx
+++ front/src/components/side.tsx
@@ -1,19 +1,36 @@
 import { A } from '@solidjs/router';
-import { ComponentProps } from 'solid-js';
+import { JSXElement, Match, Switch, createSignal } from 'solid-js';
 
 import { useSelf } from '../store';
 import { icon } from '.';
 
 import './side.css';
 
-export const Side = (props: ComponentProps<'aside'>) => {
+const Item = (props: { path: string, icon: JSXElement, text?: false | string }) =>
+  <A href={props.path}>
+    {props.icon} <span class='components_side_text' id={props.text ? 'visible' : undefined}>{props.text}</span>
+  </A>;
+
+export const Side = () => {
   const self = useSelf();
+  const [expanded, setExpanded] = createSignal(false);
 
   return (
-    <aside class='components_side' {...props}>
-      <A href='/'><icon.Trophy /></A>
-      <A href={self() ? `/user'${self()!.id}` : window.location}><icon.User /></A>
-      <A href='/'>h</A>
+    <aside class='components_side'>
+      <Item path='/' icon={<icon.Trophy />} text={expanded() && 'Leaderboard'} />
+      <Item path={self() ? `/user/${self()!.id}` : `${window.location}`} icon={<icon.User />} text={expanded() && 'User'} />
+      <Switch>
+        <Match when={expanded()}>
+          <span onClick={() => setExpanded(false)}>
+            <icon.DoubleLeft /> <span class='components_side_text' id='visible'>Collapse</span>
+          </span>
+        </Match>
+        <Match when={!expanded()}>
+          <span onClick={() => setExpanded(true)}>
+            <icon.DoubleRight />
+          </span>
+        </Match>
+      </Switch>
     </aside >
   );
 }

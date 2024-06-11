8a77401524cb5c8abc3166531c5b3464f7ec1964
Marcelo Lima <m-lima@users.noreply.github.com>
2024-06-11 15:38:21 +0200

    WIP leaderboard

--- front/src/router/leaderboard.tsx
+++ front/src/router/leaderboard.tsx
@@ -1,7 +1,80 @@
+import { createSignal, For, JSX, Suspense } from 'solid-js';
+
 import { usePlayers } from '../store';
+import { Player, byPosition } from '../types';
+import { icon, Loading } from '../components';
+
+import 'leaderboard.css';
+
+type PreparedPlayer = Player & {
+  icon?: () => JSX.Element;
+  class?: string;
+};
+
+const PlayerTable = (props: { players: Player[]; value: number }) => {
+  const length = props.players.length;
+  const preparedPlayers: PreparedPlayer[] = props.players.sort(byPosition).map((p, i) => {
+    if (i === 0) {
+      return { icon: icon.Crown, class: 'router-leaderboard-first', ...p };
+    }
+    if (i === 1) {
+      return { icon: icon.Medal, class: 'router-leaderboard-second', ...p };
+    }
+    if (i === 2) {
+      return { icon: icon.Certificate, class: 'router-leaderboard-third', ...p };
+    }
+    if (i === length - 4) {
+      return { icon: icon.Mosquito, ...p };
+    }
+    if (i === length - 3) {
+      return { icon: icon.Poop, ...p };
+    }
+    if (i === length - 2) {
+      return { icon: icon.Worm, ...p };
+    }
+    if (i === length - 1) {
+      return { icon: icon.Skull, ...p };
+    }
+    return p;
+  });
+  console.log('Outer', props.players);
+
+  return (
+    <div class='router-leaderboard'>
+      <table>
+        <thead>
+          <tr>
+            <th scope='col'>{props.value}</th>
+            <th scope='col'>#</th>
+            <th scope='col'>Player</th>
+            <th scope='col'>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <For each={preparedPlayers}>
+            {p => (
+              <tr class={p.class}>
+                <td class={p.class}>{p.icon !== undefined ? p.icon() : ''}</td>
+                <td>{p.position}</td>
+                <td>{p.name}</td>
+                <td>{p.score}</td>
+              </tr>
+            )}
+          </For>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export const Leaderboard = () => {
   const players = usePlayers();
+  const [value, setValue] = createSignal(0);
 
-  return <>{players.length}</>;
+  return (
+    <Suspense fallback={<Loading />}>
+      <button onClick={() => setValue(i => i + 1)}>C</button>
+      <PlayerTable players={players() || []} value={value()} />
+    </Suspense>
+  );
 };

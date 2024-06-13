f3776c0aec7a9518ec869c5458948f6b55c957e3
Marcelo Lima <m-lima@users.noreply.github.com>
2024-06-13 23:43:41 +0200

    WIP css for player

--- front/src/router/player.tsx
+++ front/src/router/player.tsx
@@ -7,6 +7,17 @@ import { icon, Loading } from '../components';
 
 import './player.css';
 
+export const Player = () => {
+  const params = useParams<{ id?: string }>();
+  const store = useStore();
+  const self = useSelf(store);
+  const players = usePlayers(store);
+  // TODO: preload matches
+  // void store.getMatches();
+
+  return <Suspense fallback={<Loading />}>{wrapRender(params.id, self(), players())}</Suspense>;
+};
+
 const NotFound = () => {
   return (
     <div class='router-player-not-found'>
@@ -31,59 +42,67 @@ const getId = (navigate: Navigator, self: number, param?: string) => {
   return id;
 };
 
-const PlayerView = (props: {
+const PlayerHeader = (props: {
   self: boolean;
   player: PlayerType;
   position: number;
   players: number;
 }) => {
+  const [name, setName] = createSignal(props.player.name);
+  const [editigName, setEditingName] = createSignal(false);
+
   return (
-    <>
-      <h1>{props.self && 'Me'}</h1>
-      <h1>
-        <Switch>
-          <Match when={props.position === 1}>
-            <span class='router-player-badge first'>
-              <icon.Crown />
-            </span>
-          </Match>
-          <Match when={props.position === 2}>
-            <span class='router-player-badge second'>
-              <icon.Medal />
-            </span>
-          </Match>
-          <Match when={props.position === 3}>
-            <span class='router-player-badge third'>
-              <icon.Certificate />
-            </span>
-          </Match>
-          <Match when={props.position === props.players - 3}>
-            <span class='router-player-badge'>
-              <icon.Mosquito />
-            </span>
-          </Match>
-          <Match when={props.position === props.players - 2}>
-            <span class='router-player-badge'>
-              <icon.Poop />
-            </span>
-          </Match>
-          <Match when={props.position === props.players - 1}>
-            <span class='router-player-badge'>
-              <icon.Worm />
-            </span>
-          </Match>
-          <Match when={props.position === props.players - 0}>
-            <span class='router-player-badge'>
-              <icon.Skull />
-            </span>
-          </Match>
-        </Switch>
-        {props.position} {props.player.name}
-      </h1>
-      <p>{props.player.email}</p>
-    </>
+    <div class='router-player-header'>
+      <Switch>
+        <Match when={props.position === 1}>
+          <span class='router-player-header-badge first'>
+            <icon.Crown />
+          </span>
+        </Match>
+        <Match when={props.position === 2}>
+          <span class='router-player-header-badge second'>
+            <icon.Medal />
+          </span>
+        </Match>
+        <Match when={props.position === 3}>
+          <span class='router-player-header-badge third'>
+            <icon.Certificate />
+          </span>
+        </Match>
+        <Match when={props.position === props.players - 3}>
+          <span class='router-player-header-badge'>
+            <icon.Mosquito />
+          </span>
+        </Match>
+        <Match when={props.position === props.players - 2}>
+          <span class='router-player-header-badge'>
+            <icon.Poop />
+          </span>
+        </Match>
+        <Match when={props.position === props.players - 1}>
+          <span class='router-player-header-badge'>
+            <icon.Worm />
+          </span>
+        </Match>
+        <Match when={props.position === props.players - 0}>
+          <span class='router-player-header-badge'>
+            <icon.Skull />
+          </span>
+        </Match>
+      </Switch>
+      <span class='router-player-header-name'>{props.player.name}</span>
+      <span class='router-player-header-score'># {props.position}</span>
+    </div>
   );
 };
+
+const PlayerStats = (props: { player: PlayerType }) => (
+  <div class='router-player-stats'>
+    <b>Wins</b>
+    {3}
+    <b>Losses</b>
+  </div>
+);
 
 const wrapRender = (param?: string, self?: number, players?: PlayerType[]) => {
   if (self === undefined || players === undefined) {
@@ -101,23 +120,21 @@ const wrapRender = (param?: string, self?: number, players?: PlayerType[]) => {
 
   return (
     <Show when={playerPosition >= 0} fallback={<NotFound />}>
-      <PlayerView
-        self={id === self}
-        player={players[playerPosition]}
-        position={playerPosition + 1}
-        players={players.length}
-      />
+      <div class='router-player'>
+        <PlayerHeader
+          self={id === self}
+          player={players[playerPosition]}
+          position={playerPosition + 1}
+          players={players.length}
+        />
+        <PlayerHeader
+          self={id === self}
+          player={players[playerPosition]}
+          position={0}
+          players={players.length}
+        />
+        <PlayerStats player={players[playerPosition]} />
+      </div>
     </Show>
   );
-};
-
-export const Player = () => {
-  const params = useParams<{ id?: string }>();
-  const store = useStore();
-  const self = useSelf(store);
-  const players = usePlayers(store);
-  // TODO: preload matches
-  // void store.getMatches();
-
-  return <Suspense fallback={<Loading />}>{wrapRender(params.id, self(), players())}</Suspense>;
 };

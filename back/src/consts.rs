1d4c3f908eca1d502c2edeacc8ebc8f9e8e2da7a
Marcelo Lima <m-lima@users.noreply.github.com>
2024-06-24 22:09:16 +0200

    Fix test user

--- back/src/consts.rs
+++ back/src/consts.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "local")]
 pub mod mock {
-    pub const USER_NAME: &str = "test@gmail.com";
-    pub const USER_EMAIL: &str = "tester";
+    pub const USER_NAME: &str = "tester";
+    pub const USER_EMAIL: &str = "test@gmail.com";
 
     pub const NAMES: [&str; 20] = [
         "Gunther Balarama",

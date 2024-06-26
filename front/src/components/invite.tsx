0fbb79331b66bc8aa5afa4f233c0e5915b475b69
Marcelo Lima <m-lima@users.noreply.github.com>
2024-06-26 13:13:34 +0200

    More reliable invite slide

--- front/src/components/invite.tsx
+++ front/src/components/invite.tsx
@@ -1,80 +1,30 @@
-import { createSignal, Match, Switch } from 'solid-js';
+import { JSXElement, createSignal } from 'solid-js';
 
 import * as util from '../util';
 import { useStore } from '../store';
 
 import './invite.css';
 
-const Initial = (props: { decider: (decision: boolean) => void }) => (
-  <div class='components-invite'>
-    <h1>You've been invited!</h1>
-    <h3>
-      In order to access <b>{util.name}</b>, you must accept the invitation
-    </h3>
-    <div class='components-invite-buttons'>
-      <button
-        onClick={() => {
-          props.decider(true);
-        }}
-      >
-        Accept
-      </button>
-      <button
-        class='secondary'
-        onClick={() => {
-          props.decider(false);
-        }}
-      >
-        Reject
-      </button>
-    </div>
-  </div>
-);
-
-const WillAccept = (props: { decider: (decision: boolean) => void }) => (
-  <div class='components-invite'>
-    <h1>Great news!</h1>
-    <h3>Just to make sure, can you accept again?</h3>
-    <div class='components-invite-buttons'>
-      <button
-        onClick={() => {
-          props.decider(true);
-        }}
-      >
-        Accept
-      </button>
-      <button
-        class='secondary'
-        onClick={() => {
-          props.decider(false);
-        }}
-      >
-        {' '}
-        Oops
-      </button>
-    </div>
-  </div>
-);
-
-const WillReject = (props: { decider: (decision: boolean) => void }) => (
-  <div class='components-invite'>
-    <h1>Too bad..</h1>
-    <h3>Just to make sure, can you reject again?</h3>
-    <div class='components-invite-buttons'>
-      <button
-        onClick={() => {
-          props.decider(true);
-        }}
-      >
-        Reject
-      </button>
-      <button
-        class='secondary'
-        onClick={() => {
-          props.decider(false);
-        }}
-      >
-        Oops
+const Prompt = (props: {
+  position?: 'left' | 'right';
+  header: string;
+  text: JSXElement;
+  ok: {
+    text: string;
+    action: () => void;
+  };
+  cancel: {
+    text: string;
+    action: () => void;
+  };
+}) => (
+  <div class={`components-invite-prompt ${props.position}`}>
+    <h1>{props.header}</h1>
+    <h3>{props.text}</h3>
+    <div class='components-invite-prompt-buttons'>
+      <button onClick={props.ok.action}>{props.ok.text}</button>
+      <button class='secondary' onClick={props.cancel.action}>
+        {props.cancel.text}
       </button>
     </div>
   </div>
@@ -85,46 +35,76 @@ export const Invite = () => {
   const store = useStore();
 
   return (
-    <div>
-      <div class={`components-invite right ${willAccept() === undefined && 'visible'}`}>
-        <Initial
-          decider={decision => {
+    <div class='components-invite' style={{ overflow: 'hidden' }}>
+      <Prompt
+        position={willAccept() !== undefined ? 'left' : undefined}
+        header="You've been invited!!"
+        text=<>
+          In order to access <b>{util.name}</b>, you must accept the invitation
+        </>
+        ok={{
+          text: 'Accept',
+          action: () => {
             if (willAccept() === undefined) {
-              setWillAccept(decision);
+              setWillAccept(true);
             }
-          }}
-        />
-      </div>
-      <div class={`components-invite left ${willAccept() === true && 'visible'}`}>
-        <WillAccept
-          decider={decision => {
+          },
+        }}
+        cancel={{
+          text: 'Reject',
+          action: () => {
+            if (willAccept() === undefined) {
+              setWillAccept(false);
+            }
+          },
+        }}
+      />
+      <Prompt
+        position={willAccept() !== true ? 'right' : undefined}
+        header='Great news!!'
+        text='Just to make sure, can you accept again?'
+        ok={{
+          text: 'Accept',
+          action: () => {
             if (willAccept() === true) {
-              if (decision) {
-                void store.invitationRsvp(true).finally(() => {
-                  window.location.reload();
-                });
-              } else {
-                setWillAccept();
-              }
+              void store.invitationRsvp(true).finally(() => {
+                window.location.reload();
+              });
             }
-          }}
-        />
-      </div>
-      <div class={`components-invite left ${willAccept() === false && 'visible'}`}>
-        <WillReject
-          decider={decision => {
+          },
+        }}
+        cancel={{
+          text: 'Oops',
+          action: () => {
+            if (willAccept() === true) {
+              setWillAccept();
+            }
+          },
+        }}
+      />
+      <Prompt
+        position={willAccept() !== false ? 'right' : undefined}
+        header='Too bad..'
+        text='Just to make sure, can you reject again?'
+        ok={{
+          text: 'Reject',
+          action: () => {
             if (willAccept() === false) {
-              if (decision) {
-                void store.invitationRsvp(false).finally(() => {
-                  window.location.reload();
-                });
-              } else {
-                setWillAccept();
-              }
+              void store.invitationRsvp(false).finally(() => {
+                window.location.reload();
+              });
             }
-          }}
-        />
-      </div>
+          },
+        }}
+        cancel={{
+          text: 'Oops',
+          action: () => {
+            if (willAccept() === false) {
+              setWillAccept();
+            }
+          },
+        }}
+      />
     </div>
   );
 };

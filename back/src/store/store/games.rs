5739bcddf75c853768e00721af04b6f3e623e7d3
Marcelo Lima <m-lima@users.noreply.github.com>
2024-07-11 12:01:42 +0200

    Break up and test the game logic

--- back/src/store/store/games.rs
+++ back/src/store/store/games.rs
@@ -88,54 +88,11 @@ impl Games<'_> {
     where
         F: Copy + Fn(f64, f64, bool, bool) -> (f64, f64),
     {
-        if player_one == player_two {
-            return Err(Error::InvalidValue("Players cannot be equal"));
-        }
-
-        if score_one == score_two {
-            return Err(Error::InvalidValue("Scores cannot be equal"));
-        }
-
-        if (score_one == 12 && score_two != 10) || (score_two == 12 && score_one != 10) {
-            return Err(Error::InvalidValue("Tie breaks require a 12x10 score"));
-        }
-
-        if (score_one == 11 && score_two >= 11) || (score_two == 11 && score_one >= 11) {
-            return Err(Error::InvalidValue("There can only be one winner"));
-        }
+        validate_game(player_one, player_two, score_one, score_two)?;
 
         let mut tx = self.pool.begin().await.map_err(Error::Query)?;
 
-        let (rating_one, rating_two) = sqlx::query!(
-            r#"
-            SELECT
-                one.rating as one,
-                two.rating as two
-            FROM
-                (
-                    SELECT
-                        rating
-                    FROM
-                        players
-                    WHERE
-                        id = $1
-                ) AS one,
-                (
-                    SELECT
-                        rating
-                    FROM
-                        players
-                    WHERE
-                        id = $2
-                ) AS two;
-            "#,
-            player_one,
-            player_two,
-        )
-        .fetch_one(tx.as_mut())
-        .await
-        .map_err(Error::Query)
-        .map(|r| (r.one, r.two))?;
+        let (rating_one, rating_two) = ratings(player_one, player_two, tx.as_mut()).await?;
 
         let game = sqlx::query_as!(
             types::Game,
@@ -234,5 +191,126 @@ impl Games<'_> {
         tx.commit().await.map_err(Error::Query)?;
 
         Ok((game, one, two))
+    }
+}
+
+fn validate_game(
+    player_one: types::Id,
+    player_two: types::Id,
+    score_one: u8,
+    score_two: u8,
+) -> Result {
+    if player_one == player_two {
+        Err(Error::InvalidValue("Players cannot be equal"))
+    } else if score_one == score_two {
+        Err(Error::InvalidValue("Scores cannot be equal"))
+    } else if (score_one == 12 && score_two != 10) || (score_two == 12 && score_one != 10) {
+        Err(Error::InvalidValue("Tie breaks require a 12x10 score"))
+    } else if (score_one == 11 && score_two >= 11) || (score_two == 11 && score_one >= 11) {
+        Err(Error::InvalidValue("There can only be one winner"))
+    } else {
+        Ok(())
+    }
+}
+
+async fn ratings<'c, 'e, E>(
+    player_one: types::Id,
+    player_two: types::Id,
+    executor: E,
+) -> Result<(f64, f64)>
+where
+    'c: 'e,
+    E: 'e + sqlx::Executor<'c, Database = sqlx::Sqlite>,
+{
+    sqlx::query!(
+        r#"
+        SELECT
+            one.rating as one,
+            two.rating as two
+        FROM
+            (
+                SELECT
+                    rating
+                FROM
+                    players
+                WHERE
+                    id = $1
+            ) AS one,
+            (
+                SELECT
+                    rating
+                FROM
+                    players
+                WHERE
+                    id = $2
+            ) AS two;
+        "#,
+        player_one,
+        player_two,
+    )
+    .fetch_one(executor)
+    .await
+    .map_err(Error::Query)
+    .map(|r| (r.one, r.two))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[sqlx::test]
+    async fn ratings(pool: sqlx::sqlite::SqlitePool) {
+        let one = sqlx::query_as!(
+            types::Player,
+            r#"
+            INSERT INTO players (
+                name,
+                email,
+                rating
+            ) VALUES (
+                'one',
+                'one',
+                100
+            ) RETURNING
+                id,
+                name,
+                email,
+                inviter,
+                rating,
+                created_ms AS "created_ms: types::Millis"
+            "#,
+        )
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+        let two = sqlx::query_as!(
+            types::Player,
+            r#"
+            INSERT INTO players (
+                name,
+                email,
+                rating
+            ) VALUES (
+                'two',
+                'two',
+                200
+            ) RETURNING
+                id,
+                name,
+                email,
+                inviter,
+                rating,
+                created_ms AS "created_ms: types::Millis"
+            "#,
+        )
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+        let (rating_one, rating_two) = super::ratings(one.id, two.id, &pool).await.unwrap();
+
+        assert!((rating_one - one.rating).abs() < f64::EPSILON);
+        assert!((rating_two - two.rating).abs() < f64::EPSILON);
     }
 }

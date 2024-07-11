7b2b37ba3485ae50561815a7248d0bcaab733411
Marcelo Lima <m-lima@users.noreply.github.com>
2024-07-11 14:28:55 +0200

    Fix invitation screen

--- front/src/pages/wrapper.tsx
+++ front/src/pages/wrapper.tsx
@@ -6,34 +6,38 @@ import { Welcome } from './welcome';
 import { Loading } from './loading';
 import { useStore } from '../store';
 
-export const Wrapper = (props: ParentProps<{ state: state.State }>) => {
+export const Wrapper = (props: ParentProps<{ state: state.State }>) => (
+  <Switch>
+    <Match when={props.state === state.Disconnected.Connecting}>
+      <Loading />
+    </Match>
+    <Match when={props.state === state.Disconnected.Unauthorized}>
+      <Unauthorized />
+    </Match>
+    <Match when={state.isDisconnected(props.state)}>
+      <GenericError />
+    </Match>
+    <Match when={true}>
+      <ErrorBoundary
+        fallback={error => {
+          console.debug(error);
+          return 'millis' in error ? <TimeOut /> : <GenericError />;
+        }}
+      >
+        <Suspense fallback=<Loading />>
+          <InviteWrapper>{props.children}</InviteWrapper>
+        </Suspense>
+      </ErrorBoundary>
+    </Match>
+  </Switch>
+);
+
+const InviteWrapper = (props: ParentProps) => {
   const self = useStore().getSelf();
 
   return (
-    <Switch>
-      <Match when={props.state === state.Disconnected.Connecting}>
-        <Loading />
-      </Match>
-      <Match when={props.state === state.Disconnected.Unauthorized}>
-        <Unauthorized />
-      </Match>
-      <Match when={state.isDisconnected(props.state)}>
-        <GenericError />
-      </Match>
-      <Match when={true}>
-        <ErrorBoundary
-          fallback={error => {
-            console.debug(error);
-            return 'millis' in error ? <TimeOut /> : <GenericError />;
-          }}
-        >
-          <Suspense fallback=<Loading />>
-            <Show when={self()?.pending !== true} fallback=<Welcome />>
-              {props.children}
-            </Show>
-          </Suspense>
-        </ErrorBoundary>
-      </Match>
-    </Switch>
+    <Show when={self()?.pending !== true} fallback=<Welcome />>
+      {props.children}
+    </Show>
   );
 };

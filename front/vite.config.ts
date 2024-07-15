7287c3edd125fb82a9c88344373839cec957ca21
Marcelo Lima <m-lima@users.noreply.github.com>
2024-07-15 09:31:06 +0200

    Enable PWA

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,31 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        short_name: 'PongElo',
+        name: 'PongElo',
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#333333',
+        background_color: '#333333',
+        icons: [
+          {
+            src: '/logo.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          }
+        ]
+      }
+    })
+  ],
 });

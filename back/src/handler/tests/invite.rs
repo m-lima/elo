7721b73c5b8d345d29556651797650404886fa3f
Marcelo Lima <m-lima@users.noreply.github.com>
2024-07-17 09:50:25 +0200

    Finish invite tests

--- back/src/handler/tests/invite.rs
+++ back/src/handler/tests/invite.rs
@@ -89,131 +89,113 @@ async fn reject(pool: sqlx::sqlite::SqlitePool) {
     assert_eq!(invited, uninvited);
 }
 
-// #[sqlx::test]
-// async fn cancel(pool: sqlx::sqlite::SqlitePool) {
-//     let (player, store) = init(&pool).await;
-//     let mut handler = RichHandler::new(&player.email, &store).await;
-//
-//     let invited = handler.invite(INVITED_NAME, INVITED_EMAIL, player.id).await;
-//
-//     handler
-//         .call_done(model::Request::Invite(model::request::Invite::Cancel(
-//             invited.id,
-//         )))
-//         .await;
-//
-//     handler.check_no_email();
-//
-//     match handler.push.try_recv().unwrap() {
-//         model::Push::Player(model::push::Player::Uninvited(uninvited)) => {
-//             assert_eq!(invited, uninvited);
-//         }
-//         p => panic!("Unexpected email: {p:?}"),
-//     }
-// }
-//
-// #[sqlx::test]
-// async fn invalid_input(pool: sqlx::sqlite::SqlitePool) {
-//     let (player, store) = init(&pool).await;
-//     let mut handler = RichHandler::new(&player.email, &store).await;
-//
-//     match handler
-//         .call_err(model::Request::Invite(model::request::Invite::Player {
-//             name: String::new(),
-//             email: String::from(INVITED_EMAIL),
-//         }))
-//         .await
-//     {
-//         model::Error::InvalidEmail(mailbox::Error::MissingName) => {}
-//         e => panic!("Unexpected error: {e:?}"),
-//     }
-//
-//     match handler
-//         .call_err(model::Request::Invite(model::request::Invite::Player {
-//             name: String::from(WHITE_SPACE),
-//             email: String::from(INVITED_EMAIL),
-//         }))
-//         .await
-//     {
-//         model::Error::InvalidEmail(mailbox::Error::MissingName) => {}
-//         e => panic!("Unexpected error: {e:?}"),
-//     }
-//
-//     match handler
-//         .call_err(model::Request::Invite(model::request::Invite::Player {
-//             name: String::from(INVITED_NAME),
-//             email: String::new(),
-//         }))
-//         .await
-//     {
-//         model::Error::InvalidEmail(mailbox::Error::Address(
-//             lettre::address::AddressError::MissingParts,
-//         )) => {}
-//         e => panic!("Unexpected error: {e:?}"),
-//     }
-//
-//     match handler
-//         .call_err(model::Request::Invite(model::request::Invite::Player {
-//             name: String::from(INVITED_NAME),
-//             email: String::from(WHITE_SPACE),
-//         }))
-//         .await
-//     {
-//         model::Error::InvalidEmail(mailbox::Error::Address(
-//             lettre::address::AddressError::MissingParts,
-//         )) => {}
-//         e => panic!("Unexpected error: {e:?}"),
-//     }
-// }
-//
-// #[sqlx::test]
-// async fn repeated_input(pool: sqlx::sqlite::SqlitePool) {
-//     let (player, store) = init(&pool).await;
-//     let mut handler = RichHandler::new(&player.email, &store).await;
-//
-//     handler.invite(INVITED_NAME, INVITED_EMAIL, player.id).await;
-//
-//     match handler
-//         .call_err(model::Request::Invite(model::request::Invite::Player {
-//             name: String::from(TESTER_NAME),
-//             email: String::from(TESTER_EMAIL),
-//         }))
-//         .await
-//     {
-//         model::Error::Store(store::Error::AlreadyExists) => {}
-//         e => panic!("Unexpected error: {e:?}"),
-//     }
-//
-//     match handler
-//         .call_err(model::Request::Invite(model::request::Invite::Player {
-//             name: format!("{WHITE_SPACE}{TESTER_NAME}{WHITE_SPACE}"),
-//             email: format!("{WHITE_SPACE}tEsTEr@eMAil.cOm{WHITE_SPACE}"),
-//         }))
-//         .await
-//     {
-//         model::Error::Store(store::Error::AlreadyExists) => {}
-//         e => panic!("Unexpected error: {e:?}"),
-//     }
-//
-//     match handler
-//         .call_err(model::Request::Invite(model::request::Invite::Player {
-//             name: String::from(INVITED_NAME),
-//             email: String::from(INVITED_EMAIL),
-//         }))
-//         .await
-//     {
-//         model::Error::Store(store::Error::AlreadyExists) => {}
-//         e => panic!("Unexpected error: {e:?}"),
-//     }
-//
-//     match handler
-//         .call_err(model::Request::Invite(model::request::Invite::Player {
-//             name: format!("{WHITE_SPACE}invited{WHITE_SPACE}"),
-//             email: format!("{WHITE_SPACE}iNviTeD@eMAil.cOm{WHITE_SPACE}"),
-//         }))
-//         .await
-//     {
-//         model::Error::Store(store::Error::AlreadyExists) => {}
-//         e => panic!("Unexpected error: {e:?}"),
-//     }
-// }
+#[sqlx::test]
+async fn cancel(pool: sqlx::sqlite::SqlitePool) {
+    let mut handler = init!(pool).2;
+    let invited = handler.invite(INVITED_NAME, INVITED_EMAIL).await.unwrap();
+
+    let model::Push::Player(model::push::Player::Uninvited(uninvited)) = handler
+        .call(model::Request::Invite(model::request::Invite::Cancel(
+            invited.id,
+        )))
+        .await
+        .done()
+        .unwrap()
+        .none()
+        .unwrap()
+        .some()
+        .unwrap()
+    else {
+        panic!()
+    };
+
+    assert_eq!(invited, uninvited);
+}
+
+#[sqlx::test]
+async fn invalid_input(pool: sqlx::sqlite::SqlitePool) {
+    let mut handler = init!(pool).2;
+
+    handler
+        .call(model::Request::Invite(model::request::Invite::Player {
+            name: String::new(),
+            email: String::from(INVITED_EMAIL),
+        }))
+        .await
+        .err(model::Error::InvalidEmail(mailbox::Error::MissingName))
+        .unwrap();
+
+    handler
+        .call(model::Request::Invite(model::request::Invite::Player {
+            name: String::from(WHITE_SPACE),
+            email: String::from(INVITED_EMAIL),
+        }))
+        .await
+        .err(model::Error::InvalidEmail(mailbox::Error::MissingName))
+        .unwrap();
+
+    handler
+        .call(model::Request::Invite(model::request::Invite::Player {
+            name: String::from(INVITED_NAME),
+            email: String::new(),
+        }))
+        .await
+        .err(model::Error::InvalidEmail(mailbox::Error::Address(
+            lettre::address::AddressError::MissingParts,
+        )))
+        .unwrap();
+
+    handler
+        .call(model::Request::Invite(model::request::Invite::Player {
+            name: String::from(INVITED_NAME),
+            email: String::from(WHITE_SPACE),
+        }))
+        .await
+        .err(model::Error::InvalidEmail(mailbox::Error::Address(
+            lettre::address::AddressError::MissingParts,
+        )))
+        .unwrap();
+}
+
+#[sqlx::test]
+async fn repeated_input(pool: sqlx::sqlite::SqlitePool) {
+    let mut handler = init!(pool).2;
+
+    handler.invite(INVITED_NAME, INVITED_EMAIL).await.unwrap();
+
+    handler
+        .call(model::Request::Invite(model::request::Invite::Player {
+            name: String::from(TESTER_NAME),
+            email: String::from(TESTER_EMAIL),
+        }))
+        .await
+        .err(model::Error::Store(store::Error::AlreadyExists))
+        .unwrap();
+
+    handler
+        .call(model::Request::Invite(model::request::Invite::Player {
+            name: format!("{WHITE_SPACE}{TESTER_NAME}{WHITE_SPACE}"),
+            email: format!("{WHITE_SPACE}tEsTEr@eMAil.cOm{WHITE_SPACE}"),
+        }))
+        .await
+        .err(model::Error::Store(store::Error::AlreadyExists))
+        .unwrap();
+
+    handler
+        .call(model::Request::Invite(model::request::Invite::Player {
+            name: String::from(INVITED_NAME),
+            email: String::from(INVITED_EMAIL),
+        }))
+        .await
+        .err(model::Error::Store(store::Error::AlreadyExists))
+        .unwrap();
+
+    handler
+        .call(model::Request::Invite(model::request::Invite::Player {
+            name: format!("{WHITE_SPACE}invited{WHITE_SPACE}"),
+            email: format!("{WHITE_SPACE}iNviTeD@eMAil.cOm{WHITE_SPACE}"),
+        }))
+        .await
+        .err(model::Error::Store(store::Error::AlreadyExists))
+        .unwrap();
+}

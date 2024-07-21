c683188b3bcd8ea8e1dcb5d7c0ebeb6c77a54f4a
Marcelo Lima <m-lima@users.noreply.github.com>
2024-07-21 18:07:01 +0200

    Update migration script

--- back/migrate.sh
+++ back/migrate.sh
@@ -11,18 +11,17 @@ fi
 input="${1}"
 output="${2}"
 
-# Players
-players=$(sqlite3 "${input}" 'SELECT id, name, email, inviter, created_ms FROM players;' | awk -F'|' '{ print "("$1",\""$2"\",\""$3"\","($4 == "" ? "NULL" : $4)","$5")," }')
-# sqlite3 "${output}" "INSERT INTO players (id, name, email, inviter, created_ms) VALUES ${values%,};"
+function v0 {
+  # Players
+  players=$(sqlite3 "${input}" 'SELECT id, name, email, inviter, created_ms FROM players;' | awk -F'|' '{ print "("$1",\""$2"\",\""$3"\","($4 == "" ? "NULL" : $4)","$5")," }')
 
-# Invites
-invites=$(sqlite3 "${input}" 'SELECT id, name, email, inviter, created_ms FROM invites;' | awk -F'|' '{ print "("$1",\""$2"\",\""$3"\","$4","$5")," }')
-# sqlite3 "${output}" "INSERT INTO invites (id, name, email, inviter, created_ms) VALUES ${values%,};"
+  # Invites
+  invites=$(sqlite3 "${input}" 'SELECT id, name, email, inviter, created_ms FROM invites;' | awk -F'|' '{ print "("$1",\""$2"\",\""$3"\","$4","$5")," }')
 
-# Games
-games=$(sqlite3 "${input}" 'SELECT id, player_one, player_two, score_one, score_two, challenge, created_ms FROM games;' | awk -F'|' '{ print "("$1","$2","$3","$4","$5","$6","$7",0,0,0)," }')
+  # Games
+  games=$(sqlite3 "${input}" 'SELECT id, player_one, player_two, score_one, score_two, challenge, created_ms FROM games;' | awk -F'|' '{ print "("$1","$2","$3","$4","$5","$6","$7",0,0,0)," }')
 
-sqlite3 "${output}" <<EOF
+  sqlite3 "${output}" <<EOF
 BEGIN TRANSACTION;
 INSERT INTO players (id, name, email, inviter, created_ms) VALUES ${players%,};
 INSERT INTO invites (id, name, email, inviter, created_ms) VALUES ${invites%,};
@@ -30,4 +29,36 @@ INSERT INTO games (id, player_one, player_two, score_one, score_two, challenge, 
 COMMIT;
 EOF
 
-rm "${input}-shm" "${input}-wal" "${output}-shm" "${output}-wal" || true
+  rm "${input}-shm" "${input}-wal" "${output}-shm" "${output}-wal" || true
+}
+
+function v1 {
+  # Players
+  players=$(sqlite3 "${input}" 'SELECT id, name, email, inviter, created_ms FROM players;' | awk -F'|' '{ print "("$1",\""$2"\",\""$3"\","($4 == "" ? "NULL" : $4)","$5")," }')
+
+  # Invites
+  invites=$(sqlite3 "${input}" 'SELECT id, name, email, inviter, created_ms FROM invites;' | awk -F'|' '{ print "("$1",\""$2"\",\""$3"\","$4","$5")," }')
+
+  # Games
+  games=$(sqlite3 "${input}" 'SELECT id, player_one, player_two, score_one, score_two, challenge, created_ms FROM games;' | awk -F'|' '{ print "("$1","$2","$3","$4","$5","$6","$7","$7",0,0,0,false)," }')
+
+  sqlite3 "${output}" <<EOF
+BEGIN TRANSACTION;
+INSERT INTO players (id, name, email, inviter, created_ms) VALUES ${players%,};
+INSERT INTO invites (id, name, email, inviter, created_ms) VALUES ${invites%,};
+INSERT INTO games (id, player_one, player_two, score_one, score_two, challenge, created_ms, millis, rating_one, rating_two, rating_delta, deleted) VALUES ${games%,};
+COMMIT;
+EOF
+
+  rm "${input}-shm" "${input}-wal" "${output}-shm" "${output}-wal" || true
+}
+
+
+case ${3} in
+  0) v0 ;;
+  1) v1 ;;
+  *)
+    echo 'Missing version' >&2
+    exit 1
+    ;;
+esac
